ae8e4ea234d2664eabb7ed58b5150306f3f0a933
aaronlyy <aaronlevican@gmail.com>
2026-05-12 22:27:33 +0200

    refactor: variable names are now shorter and cleaner

--- internal/account/handler.go
+++ internal/account/handler.go
@@ -9,87 +9,87 @@ import (
 )
 
 type handler struct {
-	service *service
+	s *service
 }
 
-func newHandler(service *service) *handler {
-	return &handler{service: service}
+func newHandler(s *service) *handler {
+	return &handler{s: s}
 }
 
-func (handler *handler) create(w http.ResponseWriter, r *http.Request) {
-	var createDTO createDTO
-	if err := util.ParseBody(r.Body, &createDTO); err != nil {
+func (h *handler) create(w http.ResponseWriter, r *http.Request) {
+	var dto createDTO
+	if err := util.ParseBody(r.Body, &dto); err != nil {
 		response.Send(w, http.StatusBadRequest, "Could not create account", nil)
 		return
 	}
 
-	account, err := handler.service.create(r.Context(), createDTO.Username, createDTO.Password)
+	acc, err := h.s.create(r.Context(), dto.Username, dto.Password)
 	if err != nil {
 		response.Send(w, http.StatusBadRequest, "Could not create account", nil)
 		return
 	}
 
-	response.Send(w, http.StatusOK, "Account created", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account created", toAccountDTO(acc))
 }
 
-func (handler *handler) findByID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) findByID(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	account, err := handler.service.findByID(r.Context(), id)
+	acc, err := h.s.findByID(r.Context(), id)
 	if err != nil {
 		response.Send(w, http.StatusNotFound, "Account not found", nil)
 		return
 	}
 
-	response.Send(w, http.StatusOK, "Account found", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account found", toAccountDTO(acc))
 }
 
-func (handler *handler) deleteByID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) deleteByID(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	account, err := handler.service.deleteByID(r.Context(), id)
+	acc, err := h.s.deleteByID(r.Context(), id)
 	if err != nil {
 		response.Send(w, http.StatusNotFound, "Account not found", nil)
 		return
 	}
 
-	response.Send(w, http.StatusOK, "Account deleted", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account deleted", toAccountDTO(acc))
 }
 
-func (handler *handler) deactivateByID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) deactivateByID(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	account, err := handler.service.deactivateByID(r.Context(), id)
+	acc, err := h.s.deactivateByID(r.Context(), id)
 	if err != nil {
 		response.Send(w, http.StatusNotFound, "Account not found", nil)
 		return
 	}
 
-	response.Send(w, http.StatusOK, "Account deactivated", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account deactivated", toAccountDTO(acc))
 }
 
-func (handler *handler) activateByID(w http.ResponseWriter, r *http.Request) {
+func (h *handler) activateByID(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
-	account, err := handler.service.activateByID(r.Context(), id)
+	acc, err := h.s.activateByID(r.Context(), id)
 	if err != nil {
 		response.Send(w, http.StatusNotFound, "Account not found", nil)
 		return
 	}
 
-	response.Send(w, http.StatusOK, "Account activated", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account activated", toAccountDTO(acc))
 }
 
-func (handler *handler) login(w http.ResponseWriter, r *http.Request) {
-	var loginDTO loginDTO
-	if err := util.ParseBody(r.Body, &loginDTO); err != nil {
+func (h *handler) login(w http.ResponseWriter, r *http.Request) {
+	var dto loginDTO
+	if err := util.ParseBody(r.Body, &dto); err != nil {
 		response.Send(w, http.StatusBadRequest, "Could not login", nil)
 		return
 	}
 
 	ip := util.GetClientIP(r)
 
-	account, accessToken, refreshToken, err := handler.service.login(r.Context(), loginDTO.Username, loginDTO.Password, r.UserAgent(), ip)
+	acc, accessToken, refreshToken, err := h.s.login(r.Context(), dto.Username, dto.Password, r.UserAgent(), ip)
 	if err != nil {
 		response.Send(w, http.StatusBadRequest, "Could not login", nil)
 		return
@@ -99,11 +99,11 @@ func (handler *handler) login(w http.ResponseWriter, r *http.Request) {
 	res.SetStatus(201)
 	res.SetSimpleCookie("access_token", accessToken)
 	res.SetSimpleCookie("refresh_token", refreshToken)
-	res.SetBody("Account logged in", toAccountDTO(account))
+	res.SetBody("Account logged in", toAccountDTO(acc))
 	res.Send()
 }
 
-func (handler *handler) refresh(w http.ResponseWriter, r *http.Request) {
+func (h *handler) refresh(w http.ResponseWriter, r *http.Request) {
 	// get cookie
 	cookie, err := r.Cookie("refresh_token")
 	if errors.Is(err, http.ErrNoCookie) {
@@ -117,7 +117,7 @@ func (handler *handler) refresh(w http.ResponseWriter, r *http.Request) {
 
 	// create new refresh token
 	ip := util.GetClientIP(r)
-	refreshToken, jwt, err := handler.service.refresh(r.Context(), cookie.Value, r.UserAgent(), ip)
+	refreshToken, jwt, err := h.s.refresh(r.Context(), cookie.Value, r.UserAgent(), ip)
 	if err != nil {
 		response.Send(w, 500, "Could not refresh", nil)
 		return
@@ -132,11 +132,11 @@ func (handler *handler) refresh(w http.ResponseWriter, r *http.Request) {
 	res.Send()
 }
 
-func (handler *handler) logout(w http.ResponseWriter, r *http.Request) {
+func (h *handler) logout(w http.ResponseWriter, r *http.Request) {
 	// revoke refresh token in db if cookie exists
 	cookie, err := r.Cookie("refresh_token")
 	if err == nil && cookie.Value != "" {
-		handler.service.revokeRefreshToken(r.Context(), cookie.Value)
+		h.s.revokeRefreshToken(r.Context(), cookie.Value)
 	}
 
 	res := response.NewBuilder(w)
@@ -147,11 +147,11 @@ func (handler *handler) logout(w http.ResponseWriter, r *http.Request) {
 	res.Send()
 }
 
-func (handler *handler) me(w http.ResponseWriter, r *http.Request) {
-	account, err := handler.service.me(r.Context())
+func (h *handler) me(w http.ResponseWriter, r *http.Request) {
+	acc, err := h.s.me(r.Context())
 	if err != nil {
 		response.Send(w, http.StatusInternalServerError, "Unable to load account.", nil)
 		return
 	}
-	response.Send(w, http.StatusOK, "Account found.", toAccountDTO(account))
+	response.Send(w, http.StatusOK, "Account found.", toAccountDTO(acc))
 }
